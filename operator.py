fe525c1d8f063db01fa652f24e5ddbb177e93e34
handahyun <dh.han414@gmail.com>
2026-02-14 03:40:34 +0900

    operator

---
+++ operator.py
@@ -0,0 +1,32 @@
+a = 3
+b = 4
+
+# 덧셈 7
+print(a + b)
+
+# 뺄셈 -1
+print(a - b)
+
+# 곱셈 12
+print(a * b)
+
+#나눗셈 0.75
+print(a / b)
+
+
+# a의 b제곱 81
+print(a ** b)
+
+
+# a 나누기 b의 나머지 -> 3/4=0...3
+print(a % b)
+
+#b 나누기 a의 나머지 -> 4/3=1...1
+print(b % a)
+
+
+# a 나누기 b의 몫 -> 0
+print(a // b)
+
+#b 나누기 a의 몫 -> 1
+print(b // a)
