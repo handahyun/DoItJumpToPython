73b67b93d1ace756767a3c93ffc01a20123ba213
handahyun <dh.han414@gmail.com>
2026-02-16 04:29:34 +0900

    quotes

---
+++ quotes.py
@@ -0,0 +1,35 @@
+# 문자열에 작은 따옴표(') 포함
+### cat's loved wool
+print("cat's loved wool")
+
+# 문자열에 큰 따옴표 (") 포함
+### "I like wool." he says.
+print('"I like wool." he says.')
+
+# 백슬래시 (\) 를 통해 포함
+### cat's loved wool
+###"I like wool". he says.
+cat = print("cat\'s loved wool")
+cat = print("\"I like wool\". he says.")
+
+
+### young dog is puppy.
+### young cat is kitten.
+
+# 이스케이프 코드 (\n)로 줄바꿈
+multiline = print("young dog is puppy.\nyoung cat is kitten.")
+
+# 연속된 따옴표 3개 (''' 또는 """)로 줄바꿈
+multiline = print(
+    """
+young dog is puppy.
+young cat is kitten.
+"""
+)
+
+multiline = print(
+    """
+young dog is puppy.
+young cat is kitten.
+"""
+)
