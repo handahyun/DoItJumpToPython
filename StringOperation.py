66e01b5882beb563a1c76cfa8bb1e07af3293e61
handahyun <dh.han414@gmail.com>
2026-02-16 04:32:58 +0900

    StringOperation

---
+++ StringOperation.py
@@ -0,0 +1,17 @@
+head = "I like "
+tail = "you"
+
+# 문자열 더하기
+### I like you
+print(head + tail)
+
+# 문자열 곱하기
+### veryvery
+a = "very"
+print(a * 2)
+
+
+# len 함수 : 문자열 길이 구하기 - 공백 문자도 포함
+### 21
+string = "I love my cat and dog"
+print(len(string))
